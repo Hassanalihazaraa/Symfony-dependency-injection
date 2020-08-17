5ba011dd355f65f2f43af51132dc89deaa1f68df
Hassan <hassanalihazaraa@gmail.com>
2020-08-17 16:15:19 +0200

    fixed capitalization

--- src/classes/Capitalize.php
+++ src/classes/Capitalize.php
@@ -9,7 +9,6 @@ class Capitalize implements Transform
 {
     public function transform(string $string): string
     {
-        $words = "hello world";
-        return preg_replace_callback('/(\w)(.?)/', fn() => $this->transform(strtoupper($words[2]) . $words[1]), $words);
+        return preg_replace_callback('/(\w)(.?)/', fn($words) => strtoupper($words[2] . $words[1]), $string);
     }
 }
